3f2ac32d320fdec4aa56f7cd384254d2a3c245d8
Ezekiel Grosfeld <ezekielgrosfeld@pop-os.localdomain>
2018-12-30 23:57:13 -0300

    Segunda Beta

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :recipes
   resources :categories
-  devise_for :users, :controllers => {registrations:'registrations'}
+  resources :users
+  devise_for :users, :controllers => {registrations:'registrations'}, :path => 'u'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#index'
   get 'afirmaciones/registro'
