a301f92bdff63928c7bb1d4a77c505147456a382
Ezekiel Grosfeld <ezekielgrosfeld@pop-os.localdomain>
2019-01-02 01:03:37 -0300

    sadasd

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,12 +2,6 @@ class Recipe < ApplicationRecord
   has_many :line_items, inverse_of: :order
   belongs_to :user, required: true
   belongs_to :category, required: true
-  has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" },
-  :s3_protocol => 'https',
-  :s3_host_name => ENV['S3_HOST_NAME'],
-  :url => ":s3_domain_url",
-  :path => ENV['S3_PATH'],
-  :storage => 's3',
-  :s3_region => ENV['S3_REGION']
+  has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\z/
 end
