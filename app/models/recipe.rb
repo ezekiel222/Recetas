34921b56a735663b5562c818dd3c5479efc9fa1e
Ezekiel Grosfeld <ezekielgrosfeld@pop-os.localdomain>
2019-01-02 01:34:10 -0300

    adsfsf

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,4 +3,6 @@ class Recipe < ApplicationRecord
   belongs_to :user, required: true
   belongs_to :category, required: true
   has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }
+  validates_attachment_content_type :photo, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+
 end
